e8e82253af74f042455169ff9255044ac7009615
Lidor Dvir <91224903+LidorDvir1@users.noreply.github.com>
2022-06-19 11:59:52 +0300

    Added infinite scroll

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import { Home } from "pages";
+import { Home, Favorites } from "pages";
 import { ThemeProvider } from "theme";
-import NavBar from "components/App/NavBar";
+import { NavBar } from "components/App";
 
 const AppRouter = () => {
   return (
@@ -11,6 +11,7 @@ const AppRouter = () => {
         <NavBar />
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/favorites" component={Favorites} />
         </Switch>
       </Router>
     </ThemeProvider>
---
+++ src/pages/Favorites/Favorites.js
@@ -0,0 +1,22 @@
+import { Text } from "components/UI";
+
+const Favorites = () => {
+  return (
+    <>
+      <Text>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi, iste aspernatur
+        libero, aut adipisci ab, id dignissimos ipsa obcaecati at commodi quo quibusdam
+        similique! Dolor doloribus rerum sit. Perspiciatis temporibus perferendis
+        cupiditate quos! Voluptates eligendi quis molestias ea, doloremque ab natus
+        temporibus nisi repellat dolores eaque. Culpa, a doloribus ipsum at aliquid ad
+        cupiditate ex in deserunt voluptate inventore placeat laudantium. Voluptatem
+        mollitia ex accusamus voluptates illum. Minima temporibus, molestias cupiditate
+        qui soluta, quibusdam fugit assumenda atque vero odio ducimus, nostrum animi
+        beatae reiciendis saepe distinctio vitae culpa voluptates? In beatae sapiente iste
+        recusandae delectus provident illum doloremque culpa nobis.
+      </Text>
+    </>
+  );
+};
+
+export default Favorites;
